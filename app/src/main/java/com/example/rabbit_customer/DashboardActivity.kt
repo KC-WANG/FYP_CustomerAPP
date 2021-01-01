8e5462c994678f6de5a21213dfdb1f39bf9d0189
KC WANG <cleverkachun@gmail.com>
2021-01-01 18:01:46 +0800

    Bottom Nav Menu added

--- app/src/main/java/com/example/rabbit_customer/DashboardActivity.kt
+++ app/src/main/java/com/example/rabbit_customer/DashboardActivity.kt
@@ -1,11 +1,23 @@
 package com.example.rabbit_customer
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_dashboard)
+
+		val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+		val navController = findNavController(R.id.navCtrlFragment)
+
+
+		bottomNavigationView.setupWithNavController(navController)
+	}
 }
