b4202bf1ffd69a672f3e0b8041f9e06ce00dcc06
KC WANG <cleverkachun@gmail.com>
2021-01-31 17:15:38 +0800

    Login Done with Google Firebase Database

---
+++ app/src/main/java/com/example/rabbit_customer/LoginActivity.kt
@@ -0,0 +1,91 @@
+package com.example.rabbit_customer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+class LoginActivity : AppCompatActivity() {
+	private lateinit var tv_register: TextView
+	private lateinit var et_login_email: EditText
+	private lateinit var et_login_password: EditText
+	private lateinit var btn_Login: Button
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login)
+
+		et_login_email = findViewById<EditText>(R.id.et_login_email)
+		et_login_password = findViewById<EditText>(R.id.et_login_password)
+
+		tv_register = findViewById<Button>(R.id.tv_register)
+		tv_register.setOnClickListener {
+			startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+		}
+
+		btn_Login = findViewById<Button>(R.id.btn_Login)
+		btn_Login.setOnClickListener { login() }
+	}
+
+	private fun login() {
+		when {
+			TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
+				Toast.makeText(
+					this@LoginActivity,
+					"Please Enter Name.",
+					Toast.LENGTH_SHORT
+				).show()
+			}
+
+			TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
+				Toast.makeText(
+					this@LoginActivity,
+					"Please Enter E-mail.",
+					Toast.LENGTH_SHORT
+				).show()
+			}
+
+			else -> {
+
+				val email: String = et_login_email.text.toString().trim { it <= ' ' }
+				val password: String = et_login_password.text.toString().trim { it <= ' ' }
+
+				// Create an instance to register
+				FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+					.addOnCompleteListener { task ->
+
+						if (task.isSuccessful) {
+							Toast.makeText(
+								this@LoginActivity,
+								"Login Successfully.",
+								Toast.LENGTH_SHORT
+							).show()
+
+							val intent =
+								Intent(this@LoginActivity, DashboardActivity::class.java)
+							intent.flags =
+								Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+							intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+							startActivity(intent)
+							finish()
+						} else {
+							Toast.makeText(
+								this@LoginActivity,
+								task.exception!!.message.toString(),
+								Toast.LENGTH_SHORT
+							).show()
+						}
+					}
+			}
+		}
+	}
+
+}
--- app/src/main/java/com/example/rabbit_customer/RegisterActivity.kt
+++ app/src/main/java/com/example/rabbit_customer/RegisterActivity.kt
@@ -7,6 +7,7 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 
 class RegisterActivity : AppCompatActivity() {
+	lateinit var tv_login: TextView
 	lateinit var btn_Register: Button
 	lateinit var et_register_name: EditText
 	lateinit var et_register_email: EditText
@@ -26,12 +28,17 @@ class RegisterActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_register)
 
+		tv_login = findViewById<TextView>(R.id.tv_login)
 		btn_Register = findViewById<Button>(R.id.btn_Register)
+
 		et_register_name = findViewById<EditText>(R.id.et_register_name)
 		et_register_email = findViewById<EditText>(R.id.et_register_email)
 		et_register_password = findViewById<EditText>(R.id.et_register_password)
 		et_register_phone = findViewById<EditText>(R.id.et_register_phone)
 
+		tv_login.setOnClickListener {
+			startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+		}
 		btn_Register.setOnClickListener { register() }
 	}
 
@@ -76,38 +83,40 @@ class RegisterActivity : AppCompatActivity() {
 				val phone: String = et_register_phone.text.toString().trim { it <= ' ' }
 
 				// Create an instance to register
-				FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+				FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+					.addOnCompleteListener { task ->
 
-					if (task.isSuccessful) {
-						val firebaseUser: FirebaseUser = task.result!!.user!!
-						val uid = firebaseUser.uid
+						if (task.isSuccessful) {
+							val firebaseUser: FirebaseUser = task.result!!.user!!
+							val uid = firebaseUser.uid
 
-						Toast.makeText(
-							this@RegisterActivity,
-							"Registered Successfully.",
-							Toast.LENGTH_SHORT
-						).show()
+							Toast.makeText(
+								this@RegisterActivity,
+								"Registered Successfully.",
+								Toast.LENGTH_SHORT
+							).show()
 
-						val user1 = User(uid, name, email, password, phone, "customer")
-						FirebaseDatabase.getInstance().reference.child("User").child(firebaseUser.uid)
-							.setValue(user1)
+							val user1 = User(uid, name, email, password, phone, "customer")
+							FirebaseDatabase.getInstance().reference.child("User")
+								.child(firebaseUser.uid)
+								.setValue(user1)
 
-						val intent =
-							Intent(this@RegisterActivity, DashboardActivity::class.java)
-						intent.flags =
-							Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-						intent.putExtra("user_id", firebaseUser.uid)
-						intent.putExtra("email_id", email)
-						startActivity(intent)
-						finish()
-					} else {
-						Toast.makeText(
-							this@RegisterActivity,
-							task.exception!!.message.toString(),
-							Toast.LENGTH_SHORT
-						).show()
+							val intent =
+								Intent(this@RegisterActivity, DashboardActivity::class.java)
+							intent.flags =
+								Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+							intent.putExtra("user_id", firebaseUser.uid)
+							intent.putExtra("email_id", email)
+							startActivity(intent)
+							finish()
+						} else {
+							Toast.makeText(
+								this@RegisterActivity,
+								task.exception!!.message.toString(),
+								Toast.LENGTH_SHORT
+							).show()
+						}
 					}
-				}
 			}
 		}
 	}
